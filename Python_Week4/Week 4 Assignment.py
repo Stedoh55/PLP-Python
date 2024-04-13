54bb15aef7c46074000818e536dd285bb26df8c1
Steven Simoni <157221633+Stedoh55@users.noreply.github.com>
2024-04-13 23:26:17 +0300

    The workout on week 4 Assignment

---
+++ Python_Week4/Week 4 Assignment.py
@@ -0,0 +1,11 @@
+class Person:
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+        print('person''s name is:', name)
+        print('Person''s age is:', age)
+        print('Person''s gender is:', gender)
+p = Person('Anna', 30, 'female')
+
